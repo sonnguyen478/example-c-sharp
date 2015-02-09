6ab181e6387f2e79c6f9eab1413fdf7291948e5a
GitHub Sample Student <training+githubstudents@github.com>
2015-02-09 15:52:51 -0800

    initial commit

---
+++ practice-c-sharp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="practice_c_sharp.MvcApplication" Language="C#" %>
---
+++ practice-c-sharp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(practice_c_sharp.Startup))]
+namespace practice_c_sharp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
